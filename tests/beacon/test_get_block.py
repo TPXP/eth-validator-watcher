9ffec634cc599cc85b53ea72b59958829e9db4b9
Emmanuel NALEPA <emmanuel.nalepa@kiln.fi>
2023-05-25 18:05:05 +0200

    Add tests

--- tests/beacon/test_get_block.py
+++ tests/beacon/test_get_block.py
@@ -1,9 +1,12 @@
 from pathlib import Path
-from eth_validator_watcher.beacon import Beacon
+
+from requests import Response
+from eth_validator_watcher.beacon import Beacon, NoBlockError
 from requests_mock import Mocker
 from tests.beacon import assets
 import json
-from eth_validator_watcher.models import Block
+from requests.exceptions import RetryError
+from pytest import raises
 
 
 def test_get_block_exists() -> None:
@@ -20,3 +23,15 @@ def test_get_block_exists() -> None:
         )
         block = beacon.get_block(4839775)
         assert block.data.message.proposer_index == 365100
+
+
+def test_get_block_does_not_exist() -> None:
+    def get(url: str) -> Response:
+        assert url == "http://beacon-node:5052/eth/v2/beacon/blocks/42"
+        raise RetryError
+
+    beacon = Beacon("http://beacon-node:5052")
+    beacon._Beacon__http.get = get  # type: ignore
+
+    with raises(NoBlockError):
+        beacon.get_block(42)
